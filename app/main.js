943786969f2c229cbed49df33a34c96104f63c89
rocking42 <nedpike3@gmail.com>
2017-01-26 03:31:04 +1100

    Added documentation

--- app/main.js
+++ app/main.js
@@ -5,11 +5,13 @@ const THREE = require("three");
 const d3_queue = require("d3-queue");
 const OrbitControls = require('three-orbit-controls')(THREE);
 const $ = require("jquery");
+// Utility functions
 import {
     memoize,
     debounce,
     getTween
 } from './utils';
+// Initial scene setup functions
 import {
     onWindowResize,
     canvas,
@@ -22,16 +24,19 @@ import {
     addSelected,
     removeGroups
 } from './scene';
+// 3D click functions
 import {
     raycaster,
     setEvents
 } from './events';
+// Various THREEjs helpers
 import {
     projection,
     getEventCenter,
     convertToXYZ,
     geodecoder
 } from './helpers';
+// Import the texture rendering functions
 import {
     scaleColor,
     scaleInNeed,
@@ -42,6 +47,7 @@ import {
     addMaps,
     addMapsInNeed
 } from './textureAdd';
+// Import the stacked graph data
 import {
     colorScheme,
     colorDescription,
@@ -61,6 +67,7 @@ import {
     displayNewStack,
     showStack
 } from './donatingD3'
+// Import the single area graph functions
 import {
     margin,
     height,
@@ -78,20 +85,21 @@ import {
     showLine
 } from './receivingD3'
 
-const receivingAid = ["Nigeria", "Iraq", "Afghanistan", "Pakistan", "Congo, Dem. Rep.", "Sudan", "Ethiopia", "Vietnam", "Tanzania", "Cameroon", "Mozambique", "Serbia", "Uganda", "Zambia", "West Bank and Gaza", "Indonesia", "India", "China", "Ghana", "Bangladesh", "Morocco", "Colombia", "Kenya", "Burkina Faso", "Egypt", "Mali", "Senegal", "Bolivia"];
-
-const donating = ["Australia", "Austria", "Belgium", "Canada", "Denmark", "Finland", "France", "Germany", "Greece", "Ireland", "Italy", "Japan", "Luxembourg", "Netherlands", "New Zealand", "Norway", "Portugal", "Spain", "Sweden", "Switzerland", "United Kingdom", "United States"];
-
 // Store the results in a variable
 function ready(error, results) {
     if (error) throw error;
     const [items, inNeed, dataWorld, crossSector, ecoInfraStruct, eduAid, govAndCivil, health, policies, prodSectorAid, socialServ, waterAndSanitize, countryRanking, aidReceivedAll] = [results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9], results[10], results[11], results[12], results[13]];
+    // Hard coded arrays of recipients and donaters
+    const receivingAid = ["Nigeria","Iraq","Afghanistan","Pakistan","Congo,Dem.Rep.","Sudan","Ethiopia","Vietnam","Tanzania","Cameroon","Mozambique","Serbia","Uganda","Zambia","WestBankandGaza","Indonesia","India","China","Ghana","Bangladesh","Morocco","Colombia","Kenya","BurkinaFaso","Egypt","Mali","Senegal","Bolivia"];
+
+    const donating = ["Australia","Austria","Belgium","Canada","Denmark","Finland","France","Germany","Greece","Ireland","Italy","Japan","Luxembourg","Netherlands","New Zealand","Norway","Portugal","Spain","Sweden","Switzerland","United Kingdom","United States"];
 
     let segments = 155;
-
+    // Loading screen
     d3.select("#loading").transition().duration(500)
         .style("opacity", 0).remove();
 
+    // NOT NEEDED?
     var currentCountry, overlay;
 
     // Setup cache for country textures
@@ -113,10 +121,11 @@ function ready(error, results) {
         }
     }
 
-    var textureCache = memoize(function(cntryID, color) {
-        var country = geo.find(cntryID);
-        return mapTexture(country, color);
-    });
+    // NOT NEEDED?
+    // var textureCache = memoize(function(cntryID, color) {
+    //     var country = geo.find(cntryID);
+    //     return mapTexture(country, color);
+    // });
 
     // Base globe with blue "water"
     let blueMaterial = new THREE.MeshPhongMaterial();
@@ -126,7 +135,7 @@ function ready(error, results) {
     baseGlobe.rotation.y = Math.PI;
     baseGlobe.name = "globe";
     baseGlobe.addEventListener('click', onGlobeClick);
-
+    // Grab the outline textures and add it to the scene
     const outlineTexture = mapTexture(countries)
     const worldOutline = new THREE.MeshPhongMaterial({
         map: outlineTexture,
@@ -161,54 +170,66 @@ function ready(error, results) {
       $(".btn-3d").removeClass("activeButton");
       $(this).addClass("activeButton");
     });
-
+    // Country click events
     function onGlobeClick(event) {
         // Get pointc, convert to latitude/longitude
         var latlng = getEventCenter.call(this, event);
         var country = geo.search(latlng[0], latlng[1]);
         console.log(country.code);
+        // Validate whether a country is a recipient/donater/not affected
         if (_.includes(receivingAid, country.code) && receivingAidActivated) {
+            // Update the area graph
             changeCountryLine(country.code, aidReceivedAll, "aid-received");
+            // Hide the stack graph
             d3.select("#donaterSvg").style("display", "none");
+            // Wikipedia data
             let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${country.code}&limit=1&namespace=0&format=json&callback=?`
             $.getJSON(url, function(data) {
+                // Add wiki text
                 d3.select("#d3stuff .countryInfo").text(data[2][0]).style("display", "inline-block");
             });
             d3.select("#msg").text(country.code);
-            d3.select("#stats").text(`Funds Recieved: ${country["recieved"]}$`);
+            d3.select("#stats").text(`Funds Recieved: $${country["recieved"]}`);
             d3.select(".countryRank").style("display", "block");
+            // Grap the rank of the country or ? if no data present
             let rank = (countryRanking.filter((item) => item.country === country.code))
             rank.length > 0 ? rank = rank[0].ranking : rank = "?";
             d3.select(".countryRank").text(`${rank}/96`);
         } else if (_.includes(donating, country.code) && donatersActivated) {
+            // Update the area graph
             changeCountryLine(country.code, items, "aid-given");
+            // Update the stack graph and display it if hidden
             displayNewStack(country.code, crossSector, ecoInfraStruct, eduAid, govAndCivil, health, policies, prodSectorAid, socialServ, waterAndSanitize);
+            d3.select("#donaterSvg").style("display", "inline");
+            // Hide the rank and info
             d3.select(".countryRank").style("display", "none");
             d3.select("#d3stuff .countryInfo").style("display", "none");
-            d3.select("#donaterSvg").style("display", "inline");
             d3.select("#msg").text(country.code);
             d3.select("#stats").text(`Funds Donated: ${country["aid"][2006]}`);
+            // else Helpful message
         } else if (receivingAidActivated) {
             d3.select("#msg").text(`select a reciever`);
         } else if (donatersActivated) {
             d3.select("#msg").text(`select a donator`);
         }
     }
-
+    // Add event listeners to the globe
     setEvents(camera, [baseGlobe], 'click');
     setEvents(camera, [baseGlobe], 'mousemove', 10);
-
+    // Allow the globe to be dragged around
     let controls = new OrbitControls(
         camera,
         renderer.domElement
     );
-
+    // Texture layer load
     // const donaters =  addMaps(new THREE.Group(), countries.features, "aid-given")
     // const aidLayers = addMaps(new THREE.Group(), countries.features, "aid-received")
 
     animate();
     // requestAnimationFrame(frameA);
 
+    // LAYER TOGGLES
+    // AID RECEIVE LAYERS
     let receivingAidActivated = false;
     document.querySelector(".clearMap").addEventListener("click", function() {
         //   addSelected(aidLayers);
@@ -220,7 +241,7 @@ function ready(error, results) {
             $(".rangeBarRecieving").addClass("active");
         }
     });
-
+    // AID DONATE LAYERS
     let donatersActivated = false;
     document.querySelector(".showDonate").addEventListener("click", function() {
         //   addSelected(donaters);
@@ -233,6 +254,7 @@ function ready(error, results) {
         }
     });
 
+  // Add the data to the scales
   scaleColor.domain(d3.extent(items, (d) => {
       return +d[2006];
   }));
@@ -242,11 +264,13 @@ function ready(error, results) {
           return +d[2006];
       }
   }));
-
+  // Find the area data needed for the line area graph
   const desCountry = findLineInfo("Germany", items, "aid-given")
+  // Display the initial single area Graph
   showLine(desCountry);
-
+  // Grab the data for the initial setup stack graph
   const dataset = findStackedData("Germany", crossSector, ecoInfraStruct, eduAid, govAndCivil, health, policies, prodSectorAid, socialServ, waterAndSanitize);
+  // Display the stacked graph using the data
   showStack(dataset);
 }
 // Load the data
