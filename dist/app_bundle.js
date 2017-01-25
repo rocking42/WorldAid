125ebd741c7bfba63f7a7707c24e08a7793d514c
AhiShahar <ahishahar1987@gmail.com>
2017-01-25 12:24:49 +1100

    cssing

--- dist/app_bundle.js
+++ dist/app_bundle.js
@@ -71,6 +71,10 @@
 	var OrbitControls = __webpack_require__(13)(THREE);
 	
 	
+	var receivingAid = ["Nigeria", "Iraq", "Afghanistan", "Pakistan", "Congo, Dem. Rep.", "Sudan", "Ethiopia", "Vietnam", "Tanzania", "Cameroon", "Mozambique", "Serbia", "Uganda", "Zambia", "West Bank and Gaza", "Indonesia", "India", "China", "Ghana", "Bangladesh", "Morocco", "Colombia", "Kenya", "Burkina Faso", "Egypt", "Mali", "Senegal", "Bolivia"];
+	
+	var donating = ["Australia", "Austria", "Belgium", "Canada", "Denmark", "Finland", "France", "Germany", "Greece", "Ireland", "Italy", "Japan", "Luxembourg", "Netherlands", "New Zealand", "Norway", "Portugal", "Spain", "Sweden", "Switzerland", "United Kingdom", "United States"];
+	
 	// Store the results in a variable
 	function ready(error, results) {
 	    if (error) throw error;
@@ -189,8 +193,6 @@
 	    baseGlobe.name = "globe";
 	    baseGlobe.addEventListener('click', onGlobeClick);
 	
-	    // baseGlobe.addEventListener('click', onGlobeMousemove);
-	
 	    var outlineTexture = (0, _textureAdd.mapTexture)(countries);
 	    var worldOutline = new THREE.MeshPhongMaterial({
 	        map: outlineTexture,
@@ -290,9 +292,19 @@
 	        var latlng = _helpers.getEventCenter.call(this, event);
 	
 	        var country = geo.search(latlng[0], latlng[1]);
-	        // console.log(country);
+	        // console.log(country.code);
+	        if (_.includes(receivingAid, country.code) && receivingAidActivated) {
+	            d3.select("#msg").text(country.code + " is recieving aid");
+	        } else if (_.includes(donating, country.code) && donatersActivated) {
+	            d3.select("#msg").text(country.code + " is a donator");
+	        } else if (receivingAidActivated) {
+	            d3.select("#msg").text("select a reciever");
+	        } else if (donatersActivated) {
+	            d3.select("#msg").text("select a donator");
+	        }
+	
 	        if (country) {
-	            d3.select("#msg").text(country.code);
+	            // d3.select("#msg").text(country.code);
 	            d3.select(".header").text(country.code);
 	            if (country["aid"]) {
 	                d3.select("#stats").text(country["aid"][2006]);
@@ -303,185 +315,33 @@
 	        }
 	    }
 	
-	    function onGlobeMousemove(event) {
-	        var map, material;
-	
-	        // Get pointc, convert to latitude/longitude
-	        var latlng = _helpers.getEventCenter.call(this, event);
-	
-	        // Look for country at that latitude/longitude
-	        var country = geo.search(latlng[0], latlng[1]);
-	        console.log(country);
-	        if (country !== null && country.code !== currentCountry) {
-	
-	            // Track the current country displayed
-	            currentCountry = country.code;
-	
-	            // Update the html
-	            d3.select("#msg").html(country.code);
-	
-	            // Overlay the selected country
-	            map = textureCache(country.code);
-	            material = new THREE.MeshPhongMaterial({
-	                map: map,
-	                transparent: true
-	            });
-	            // if (!overlay) {
-	            //   overlay = new THREE.Mesh(new THREE.SphereGeometry(201, 40, 40), material);
-	            //   overlay.rotation.y = Math.PI;
-	            //   root.add(overlay);
-	            // } else {
-	            //   overlay.material = material;
-	            // }
-	        }
-	    }
-	
 	    (0, _events.setEvents)(_scene.camera, [baseGlobe], 'click');
 	    (0, _events.setEvents)(_scene.camera, [baseGlobe], 'mousemove', 10);
 	
 	    var controls = new OrbitControls(_scene.camera, _scene.renderer.domElement);
 	
-	    // const donaters = addMaps(new THREE.Group(), countries.features);
-	    // const receivingAid = addMapsInNeed(new THREE.Group(), countries.features);
+	    // donaters: addMaps(new THREE.Group(), countries.features),
+	    // receivingAid: addMapsInNeed(new THREE.Group(), countries.features)
 	
 	    (0, _scene.animate)();
 	    // requestAnimationFrame(frameA);
 	
-	    // document.querySelector(".clearMap").addEventListener("click", function() {
-	    //   addSelected(receivingAid);
-	    // });
-	    // document.querySelector(".showDonate").addEventListener("click", function() {
-	    //   addSelected(donaters);
-	    // });
+	    var receivingAidActivated = false;
+	    document.querySelector(".clearMap").addEventListener("click", function () {
+	        //   addSelected(receivingAid);
+	        receivingAidActivated = true;
+	        donatersActivated = false;
+	    });
+	
+	    var donatersActivated = false;
+	    document.querySelector(".showDonate").addEventListener("click", function () {
+	        //   addSelected(donaters);
+	        donatersActivated = true;
+	        receivingAidActivated = false;
+	    });
 	}
 	// Load the data
 	d3_queue.queue().defer(d3.csv, "../assets/Data1.csv").defer(d3.csv, "../assets/Data5.csv").defer(d3.json, "../assets/world.json").awaitAll(ready);
-	
-	// function doSomething() {
-	//
-	//     var format = d3.time.format("%m/%d/%y");
-	//
-	//     var margin = {
-	//             top: 20,
-	//             right: 30,
-	//             bottom: 30,
-	//             left: 40
-	//         },
-	//         width = 960 - margin.left - margin.right,
-	//         height = 500 - margin.top - margin.bottom;
-	//
-	//     var x = d3.time.scale()
-	//         .range([0, width]);
-	//
-	//     var y = d3.scale.linear()
-	//         .range([height, 0]);
-	//
-	//     var z = d3.scale.category20c();
-	//
-	//     var xAxis = d3.svg.axis()
-	//         .scale(x)
-	//         .orient("bottom")
-	//         .ticks(d3.time.days);
-	//
-	//     var yAxis = d3.svg.axis()
-	//         .scale(y)
-	//         .orient("left");
-	//
-	//     var stack = d3.layout.stack()
-	//         .offset("zero")
-	//         .values(function(d) {
-	//             return d.values;
-	//         })
-	//         .x(function(d) {
-	//             return d.date;
-	//         })
-	//         .y(function(d) {
-	//             return d.value;
-	//         });
-	//
-	//     var nest = d3.nest()
-	//         .key(function(d) {
-	//             return d.key;
-	//         });
-	//
-	//     var area = d3.svg.area()
-	//         .interpolate("cardinal")
-	//         .x(function(d) {
-	//             return x(d.date);
-	//         })
-	//         .y0(function(d) {
-	//             return y(d.y0);
-	//         })
-	//         .y1(function(d) {
-	//             return y(d.y0 + d.y);
-	//         });
-	//
-	//     var svg = d3.select("#d3stuff").append("svg")
-	//         .attr("width", width + margin.left + margin.right)
-	//         .attr("height", height + margin.top + margin.bottom)
-	//         .append("g")
-	//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	//
-	//
-	//     function countryExtent(country, dataType) {
-	//         const res = dataType.filter((d) => d["aid-received"] === country);
-	//         const items = Object.values(res[0]).map((d) => +d);
-	//         const those = _.compact(items)
-	//         return [Math.min(...those), Math.max(...those)];
-	//     }
-	//
-	//     function countryYearsAndAid(country, dataType) {
-	//         const res = dataType.filter((d) => d["aid-received"] === country);
-	//         const money = Object.values(res[0]).map((d) => +d);
-	//         const years = Object.keys(res[0]).map((d) => d);
-	//         const yearsComp = years.splice(0, years.length - 1)
-	//         return [yearsComp, _.compact(money)];
-	//     }
-	//
-	//     d3.csv("../assets/Data3.csv", function(error, data) {
-	//         if (error) throw error;
-	//         // data.forEach(function(d) {
-	//         //     d.date = format.parse(d.date);
-	//         //     d.value = +d.value;
-	//         // });
-	//
-	//         const newData = countryYearsAndAid("Afghanistan", data);
-	//         const newYears = countryYearsAndAid[0];
-	//         const newMoney = countryYearsAndAid[1];
-	//         debugger
-	//         var layers = stack(nest.entries(data));
-	//
-	//         x.domain(d3.extent(data, function(d) {
-	//             return d.date;
-	//         }));
-	//         y.domain([0, d3.max(data, function(d) {
-	//             return d.y0 + d.y;
-	//         })]);
-	//
-	//         svg.selectAll(".layer")
-	//             .data(layers)
-	//             .enter().append("path")
-	//             .attr("class", "layer")
-	//             .attr("d", function(d) {
-	//                 return area(d.values);
-	//             })
-	//             .style("fill", function(d, i) {
-	//                 return z(i);
-	//             });
-	//
-	//         svg.append("g")
-	//             .attr("class", "x axis")
-	//             .attr("transform", "translate(0," + height + ")")
-	//             .call(xAxis);
-	//
-	//         svg.append("g")
-	//             .attr("class", "y axis")
-	//             .call(yAxis);
-	//     });
-	//     console.log("hello");
-	// }
-	//
-	// document.getElementById("doSomething").addEventListener("click", doSomething);
 
 /***/ },
 /* 2 */
